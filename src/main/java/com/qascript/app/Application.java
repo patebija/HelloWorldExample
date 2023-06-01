482cab0dc15372326abadfa8d98ca3f6d7005538
ishwar chand <ishwarchandupadhyay1975@gmail.com>
2023-06-01 18:00:55 +0530

    update the Application .java  from Hello world! to Hello world

--- src/main/java/com/qascript/app/Application.java
+++ src/main/java/com/qascript/app/Application.java
@@ -2,16 +2,17 @@ package com.qascript.app;
 
 public class Application {
 
-    private final String message = "Hello World!";
+    private final String message = "Hello World";
 
-    public Application() {}
+    public Application() {
+    }
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println(new Application().getMessage());
 
     }
 
-    public final String getMessage(){
+    public final String getMessage() {
         return message;
     }
 
